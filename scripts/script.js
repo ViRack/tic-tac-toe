49257021f4c181629427aa3e540210efcebe21c2
ViRack <vincentm_2@hotmail.com>
2023-03-10 00:10:30 -0800

    winning logic skeliton added.

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,99 @@
 var ticTacToe = (function() {
 
     const blocks        = document.getElementsByClassName("grid-block");
-    const vsAISlider    = document.getElementsByClassName("slider");
+    const vsAISlider    = document.querySelector(".slider");
+   
     let vsAI    = true;
-    player1Turn = true;
+    let player1Turn = true;
+   
+    const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const emptyBlock         = 0;
+    const player1       = 1;
+    const player2OrAI   = 2;
+    const winStreak     = 3;
 
-    render();
+    _render();
 
-    function render() {
-        setGridButtonFunctionality();
-        setVsButtonFunctionality();
+    function _render() {
+        _setGridButtonFunctionality();
+        _setVsButtonFunctionality();
     }
 
-    function setGridButtonFunctionality() {
+    function _setGridButtonFunctionality() {
         for (let i = 0; i < blocks.length; i++) {
             blocks[i].addEventListener("click", function() {
-                setMarker(blocks[i]);
+                _setMarker(blocks[i], i);
             })
         }
     } 
 
-    function setMarker(block) {
+    function clearGrid() {
+        for (let i = 0; i < blocks.length; i++) {
+            blocks[i].textContent = "";
+        }
+    }
+    
+
+    function _setMarker(block, index) {
         if (vsAI) {
-            block.textContent = "X";
+            if (board[index] === emptyBlock) {
+                block.textContent = "X";
+                board[index] = 1;
+                _aiTurn();
+                console.log(board);
+            }
+
         } else if (player1Turn) {
-            block.textContent = "X";
-            player1Turn = false;
+            if (board[index] === emptyBlock) {
+                block.textContent = "X";
+                board[index] = 1;
+                player1Turn = false;
+            }
+
         } else {
             block.textContent = "O";
+            player1Turn = true;
         }
     }
 
-    function setVsButtonFunctionality() {
-        vsAISlider[0].addEventListener("click"), function() {
-            vsAI = false;
-        }
+    function _setVsButtonFunctionality() {
+        vsAISlider.addEventListener("click", function() {
+            console.log("clicked");
+
+            if (vsAI) {
+                vsAI = false;
+            } else {
+                vsAI = true;
+            }
+
+            clearGrid();
+        })
+    }
+
+    function _aiTurn() {
+
+    }
+
+    function _minMax() {
+
+    }
+
+    function _isGameWon() {
+        
+    }
+
+    // Logic needs adjusting
+    function _isLineWon(index1, index2, index3) {
+        if ((board[index1] + board[index2] + board[index3]) === winStreak) {
+            return true;
+        }s
+
+        return false;
+    }
+
+    return {
+        clearGrid : clearGrid,
+
     }
 
 })();
